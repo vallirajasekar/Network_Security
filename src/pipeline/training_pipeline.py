49b19de76fb82b606e1d76e31196761b9c596f1a
vallirajasekar <sekarvalliraja@gmail.com>
2024-08-26 15:43:36 +0530

    Data Validation Added

--- src/pipeline/training_pipeline.py
+++ src/pipeline/training_pipeline.py
@@ -46,11 +46,26 @@ class TrainingPipeline:
         
         except Exception as e:
             raise NetworkSecurityException(e,sys)
+    
+    def start_data_validation(self,data_ingestion_artifact:DataIngestionArtifact):
+        try:
+            data_validation_config=DataValidationConfig(training_pipeline_config=self.training_pipeline_config)
+            data_validation=DataValidation(data_ingestion_artifact=data_ingestion_artifact,data_validation_config=data_validation_config)
+            data_validation_artifact=data_validation.initiate_data_validation()
+            logging.info(f"Data Validation Completed and artifact :{data_validation_artifact}")
+            return data_validation_artifact
+        except Exception as e:
+            raise NetworkSecurityException
+            
+            
+    
         
     def run_pipeline(self):
         try:
             data_ingestion_artifact=self.start_data_ingestion()
-            print(data_ingestion_artifact)
+            #print(data_ingestion_artifact)
+            data_validation_artifact=self.start_data_validation(data_ingestion_artifact=data_ingestion_artifact)
+            print(data_validation_artifact)
         except Exception as e:
             raise NetworkSecurityException(e,sys)
         
